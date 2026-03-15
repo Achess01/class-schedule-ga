31de62301ef9b15544c51c6f1f2be857aa2260c9
Alexander Tzoc <jealtzo26@gmail.com>
2026-03-15 01:32:16 -0600

    Add Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ProblemDetailFilter } from './common/errors/problem-detail.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Class Schedule API')
+    .setDescription('API for managing class schedules')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   app.useGlobalFilters(new ProblemDetailFilter());
   app.useGlobalPipes(
     new ValidationPipe({
